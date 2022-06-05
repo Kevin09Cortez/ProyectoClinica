9a3129cc5ecf9a47139295c740ef5e29a0e8777d
Kevin09Cortez <100536123+Kevin09Cortez@users.noreply.github.com>
2022-06-05 13:36:14 -0600

    Add files via upload

---
+++ clinica/Persona1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kevin
+ */
+class Persona {
+
+    static Object Alimentarse(String respuesta) 
+    {
+        respuesta = "si";
+        if(respuesta == respuesta) return "Ha comido";
+        return null;
+    }
+
+    static Object Sexo(String sx) 
+    {
+         sx = "f";
+         if (sx == sx) return "femenino";
+        return null;
+    }
+
+    static Object Nombre(String nombre) {
+        nombre = "Alexander Arana";
+        if(nombre == nombre) return "Alexander Arana";
+        return null;
+    }
+    
+}
